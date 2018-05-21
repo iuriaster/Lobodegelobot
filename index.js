f66c52d3240e5e321732764a039b6f302018980d
iuriaster <39443357+iuriaster@users.noreply.github.com>
2018-05-21 17:56:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands');
 
 client.on('ready', () => {
 	console.log('Ready!');
 });
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+	if (!message.content.startsWith(prefix) || message.author.bot) {
+		const renan = message.content.toLowerCase().split(/ +/);
+		for(i=0;i<renan.length;i++) {
+			if(renan[i] === 'renan') {
+				message.channel.send('Lobo de gelo!!');
+			}
+		}
+		return;
+	}
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
 	}
-	else if (command === 'flip') {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`You didn't provide anyone to flip, ${message.author}!`);
-		}
-		const taggedUser = message.mentions.users.first();
-		const inverted = taggedUser.username.split('').reverse().join('');
-		message.channel.send(`(╯°□°）╯︵${inverted}`);
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 client.login(process.env.BOT_TOKEN);
